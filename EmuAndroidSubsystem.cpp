045f42ba1ecd683c6d6f8bdfa4cb0332c5546705
Lillium <lilliumbruno@gmail.com>
2024-05-08 15:10:00 -0700

    initial commit AGAIN

---
+++ EmuAndroidSubsystem.cpp
@@ -0,0 +1,12 @@
+﻿// EmuAndroidSubsystem.cpp : Defines the entry point for the application.
+//
+
+#include "EmuAndroidSubsystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
